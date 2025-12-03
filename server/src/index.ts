f3d6947d36ed76378e3a5e5a47b19a667722b0e5
renatinhocg <renatinhocg@gmail.com>
2025-12-03 00:36:29 -0300

    fix: allow Range header in CORS for react-admin pagination

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Range')
   res.setHeader('Access-Control-Expose-Headers', 'Content-Range, X-Total-Count')
 
   // Handle preflight
